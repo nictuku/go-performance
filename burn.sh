70e0daf42d4d181aacefc2ec9d6bab811b54faac
Yves Junqueira <yves.junqueira@gmail.com>
2014-12-30 03:06:38 +0100

    Decent plot using colors

--- burn.sh
+++ burn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -ux
 
 # If relevant for your system, run this as root:
@@ -10,8 +11,10 @@ set -ux
 echo -n "# ---------------------" >> report.txt
 date >> report.txt
 
-for PROCS in 1 2 3; do
-	for GOROUTINES in 1 2 3 4 8; do
+for PROCS in 1 2 3 4 5 6 7 8 9 10; do
+	for GOROUTINES in 1 2 3 4 8 16; do
 		GOMAXPROCS=${PROCS} go run cpuburn.go ${GOROUTINES} >> report.txt
+		# Give it time for all connections to close.
+		sleep 5
 	done
 done
