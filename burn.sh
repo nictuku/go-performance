801034df405fd183dd367576c595689b3f9e5ff0
Yves Junqueira <yves.junqueira@gmail.com>
2014-12-30 02:17:40 +0100

    bad gnuplot

--- burn.sh
+++ burn.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -ux
 
+# If relevant for your system, run this as root:
+# for i in /sys/devices/system/cpu/cpu[0-9]*
+# do
+# 	    echo performance > $i/cpufreq/scaling_governor
+# done
+
 echo -n "# ---------------------" >> report.txt
 date >> report.txt
 
 for PROCS in 1 2 3; do
-	for GOROUTINES in 1 2 4 8; do
+	for GOROUTINES in 1 2 3 4 8; do
 		GOMAXPROCS=${PROCS} go run cpuburn.go ${GOROUTINES} >> report.txt
 	done
 done
