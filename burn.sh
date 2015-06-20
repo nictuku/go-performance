b99e564abccaaf7ebfd21638c4a3bf8cd128852e
Yves Junqueira <yves.junqueira@gmail.com>
2015-06-20 22:09:50 +0200

    report fixes

--- burn.sh
+++ burn.sh
@@ -8,10 +8,14 @@ set -ux
 # 	    echo performance > $i/cpufreq/scaling_governor
 # done
 
+go build || exit 1
+
 echo -n "# ---------------------" >> report.txt
+go version >> report.txt
+echo -n "# ----" >> report.txt
 date >> report.txt
 
-for PROCS in 1 2 3 4 5 6 7 8 9 10; do
+for PROCS in $(seq 1 2); do
 	for GOROUTINES in 1 2 3 4 8 16; do
 		GOMAXPROCS=${PROCS} ./go-performance ${GOROUTINES} >> report.txt
 		# Give it time for all connections to close.
